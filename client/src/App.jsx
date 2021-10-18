b797812a8690b3c7cb6a8c4a8ed3420257454571
monkeymedic26 <monkeymedic26@gmail.com>
2021-10-18 10:21:55 -0700

    added Nav component init.  added some global CSS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
+import Nav from './Nav';
+
+const PageStyle = styled.div`
+height: 100%;
+background: #28292B;
+  color: white;
+`;
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <PageStyle>
+      <Nav />
+    </PageStyle>
   )
 }
 
---
+++ client/src/Nav.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from "styled-components";
+
+const CompStyle = styled.div`
+  background: #52275C;
+  color: #CA61E1;
+`;
+
+const Nav = () => {
+  return (
+    <CompStyle>
+      <p>Hello World</p>
+    </CompStyle>
+  )
+}
+
+export default Nav;
