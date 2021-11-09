54b65f8ae0ab4569d9cf04472beee88554cd8ea0
monkeymedic26 <monkeymedic26@gmail.com>
2021-11-08 22:42:11 -0800

    init Achievements section

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import styled from "styled-components";
 
 const CompStyle = styled.div`
-  background: 28292B;
+  background: #28292B;
 `;
 
 const Intro = styled.div`
   background: #52275C;
   height: 175px;
-  color: white;
   text-align: center;
 `;
 
@@ -43,6 +42,11 @@ const Bio = styled.p`
   width: 60%;
 `;
 
+const Achievements = styled.div`
+  margin: 0px;
+  padding: 20px;
+`;
+
 const Home = () => {
   return (
     <CompStyle>
@@ -51,9 +55,12 @@ const Home = () => {
         <Title>Software Engineer</Title>
       </Intro>
       <BioSection>
-      <BioPic src="https://i.ibb.co/RBG42fN/1516873872556.jpg" alt="1516873872556" border="0" />
+        <BioPic src="https://i.ibb.co/RBG42fN/1516873872556.jpg" alt="1516873872556" border="0" />
         <Bio>My Name is Alex Duncan. I am a full stack engineer that takes pride in everything I do. That includes getting stuck in my code. Getting stuck gives me the opportunity to learn even more and expand my knowledge of the particular problem area. My passion is frontend projects as I get to utilize my Graphic Design background. I love bringing my projects to life on the client facing side.<br /> My dream is to work in AR/VR some day. I just think it's amazing the things we can already do in those fields and I want to be part of the innovation that is still to come. My interest in this field mostly stems from my training in 3D modeling and sculpture.  I love working on projects that force me to think outside what is right in front of me and see the bigger picture.</Bio>
       </BioSection>
+      <Achievements>
+        <p>Hello World!</p>
+      </Achievements>
     </CompStyle>
   )
 }
