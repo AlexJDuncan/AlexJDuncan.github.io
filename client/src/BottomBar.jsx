44ec52b2bda5f607d04e4393ac0a9eedd1d64b02
monkeymedic26 <monkeymedic26@gmail.com>
2021-11-08 20:52:19 -0800

    added github link and image

--- client/src/BottomBar.jsx
+++ client/src/BottomBar.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const BarStyle = styled.div`
+  background: #52275C;
+`;
 
 const BottomBar = () => {
   return(
-    <div>
-      <p>Hello World</p>
-    </div>
+    <BarStyle>
+      <p>Hello World!</p>
+      <a href="https://github.com/monkeymedic26">
+        <img src="https://img.icons8.com/material-outlined/96/000000/github.png"/>
+      </a>
+      
+    </BarStyle>
   )
 }
 
 export default BottomBar;
+
+// react router for link with image
+{/* <Link to="https://github.com/Joeyryanbridges">
+  <Image src="giticon.png" className="githubIcon" />
+</Link> */}
