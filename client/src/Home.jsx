7d7ebc796c4e7744e58cb3c086d231e7684cead3
monkeymedic26 <monkeymedic26@gmail.com>
2021-11-09 15:36:33 -0800

    added images for UNCO and HR and styled the achievements more

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from "styled-components";
 import Army from "./images/Army.png";
+import UNCO from "./images/UNC-Logo.png";
+import HackReactor from "./images/HackReactor.png";
 
 const CompStyle = styled.div`
   background: #28292B;
@@ -44,6 +46,8 @@ const Bio = styled.p`
 `;
 
 const AchievementsStyle = styled.div`
+  display: flex;
+  justify-content: space-around;
   margin: 0px;
   padding: 20px;
 `;
@@ -52,6 +56,13 @@ const AchievementsTitle = styled.p`
   color: #CA61E1;
   font-size: 2.5rem;
   text-align: center;
+  display: block;
+`;
+
+const AchievementsPic = styled.img`
+  height: 200px;
+  margin-right: 25px;
+  margin-left: 25px;
 `;
 
 const Home = () => {
@@ -65,9 +76,11 @@ const Home = () => {
         <BioPic src="https://i.ibb.co/RBG42fN/1516873872556.jpg" alt="1516873872556" border="0" />
         <Bio>My Name is Alex Duncan. I am a full stack engineer that takes pride in everything I do. That includes getting stuck in my code. Getting stuck gives me the opportunity to learn even more and expand my knowledge of the particular problem area. My passion is frontend projects as I get to utilize my Graphic Design background. I love bringing my projects to life on the client facing side.<br /> My dream is to work in AR/VR some day. I just think it's amazing the things we can already do in those fields and I want to be part of the innovation that is still to come. My interest in this field mostly stems from my training in 3D modeling and sculpture.  I love working on projects that force me to think outside what is right in front of me and see the bigger picture.</Bio>
       </BioSection>
+      <AchievementsTitle>Achievements</AchievementsTitle>
       <AchievementsStyle>
-        <AchievementsTitle>Achievements</AchievementsTitle>
-        <img src={Army} alt="Army Logo" border="0"></img>
+        <AchievementsPic src={Army} alt="Army Logo" />
+        <AchievementsPic src={UNCO} alt="UNCO Logo" />
+        <AchievementsPic src={HackReactor} alt="Hack Reactor Logo" />
       </AchievementsStyle>
     </CompStyle>
   )
