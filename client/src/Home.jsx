0f902e378449b6ded2a2e3cde83de75eb401e62b
monkeymedic26 <monkeymedic26@gmail.com>
2021-11-08 20:17:02 -0800

    added profile pic

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -29,7 +29,8 @@ const BioSection = styled.div`
 `;
 
 const BioPic = styled.img`
-  width: 30%;
+  width: 20%;
+  margin: 50px;
   display: inline-block;
 `;
 
@@ -48,7 +49,7 @@ const Home = () => {
         <Title>Software Engineer</Title>
       </Intro>
       <BioSection>
-        <BioPic src="" alt="" />
+      <BioPic src="https://i.ibb.co/RBG42fN/1516873872556.jpg" alt="1516873872556" border="0" />
         <Bio>My Name is Alex Duncan. I am a full stack engineer that takes pride in everything I do. That includes getting stuck in my code. Getting stuck gives me the opportuniity learn even more and expand my knowledge of the particular problem area. My passion is frontend the of a project as I can get to utilize my Graphic Design background. I love bringing my projects to life on the client facing side. My dream is to work in AR/VR some day. I just think it's amazing the things we can already do in thos fields and I want to be part of the innovation that is still to come.</Bio>
       </BioSection>
     </CompStyle>
