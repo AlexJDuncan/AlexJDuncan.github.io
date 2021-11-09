5d8300fcfc452771d2b57defdb559d6fd53ab5a6
monkeymedic26 <monkeymedic26@gmail.com>
2021-11-09 06:37:01 -0800

    added title for achievements section

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -36,15 +36,21 @@ const BioPic = styled.img`
 
 const Bio = styled.p`
   color: #CA61E1;
-  font-size: 2rem;
+  font-size: 1.5rem;
   display: inline-block;
   vertical-align: top;
   width: 60%;
 `;
 
-const Achievements = styled.div`
+const AchievementsStyle = styled.div`
   margin: 0px;
   padding: 20px;
+`;
+
+const AchievementsTitle = styled.p`
+  color: #CA61E1;
+  font-size: 2.5rem;
+  text-align: center;
 `;
 
 const Home = () => {
@@ -58,9 +64,9 @@ const Home = () => {
         <BioPic src="https://i.ibb.co/RBG42fN/1516873872556.jpg" alt="1516873872556" border="0" />
         <Bio>My Name is Alex Duncan. I am a full stack engineer that takes pride in everything I do. That includes getting stuck in my code. Getting stuck gives me the opportunity to learn even more and expand my knowledge of the particular problem area. My passion is frontend projects as I get to utilize my Graphic Design background. I love bringing my projects to life on the client facing side.<br /> My dream is to work in AR/VR some day. I just think it's amazing the things we can already do in those fields and I want to be part of the innovation that is still to come. My interest in this field mostly stems from my training in 3D modeling and sculpture.  I love working on projects that force me to think outside what is right in front of me and see the bigger picture.</Bio>
       </BioSection>
-      <Achievements>
-        <p>Hello World!</p>
-      </Achievements>
+      <AchievementsStyle>
+        <AchievementsTitle>Achievements</AchievementsTitle>
+      </AchievementsStyle>
     </CompStyle>
   )
 }
