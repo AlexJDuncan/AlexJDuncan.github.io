a73ca626e74877be5bd80694cef2daf1ca777aeb
monkeymedic26 <monkeymedic26@gmail.com>
2022-02-09 13:21:16 -0800

    Added name section in Entry

--- client/src/Entry.jsx
+++ client/src/Entry.jsx
@@ -1,12 +1,26 @@
 import React from "react";
+import styled from "styled-components";
+
+const Letter = styled.p `
+  display: inline-block;
+  color: #CA61E1;
+  font-size: 10rem;
+  margin: .5rem;
+`;
+
+const NamePart = styled.p`
+  display: inline-block;
+  font-size: 5rem;
+  margin: .5rem;
+`;
 
 const Entry = () => {
   return (
     <div>
-      <p display='inline-block'>A</p>
-      <p>lex</p>
-      <p>D</p>
-      <p>uncan</p>
+      <Letter>A</Letter>
+      <NamePart>lex</NamePart><br />
+      <Letter>D</Letter>
+      <NamePart>uncan</NamePart>
     </div>
   )
 }
