fcaba19777c3c11a3d736f727b40c894104ec358
monkeymedic26 <monkeymedic26@gmail.com>
2022-02-12 15:26:29 -0800

    added grid to Contact and moved text to appropriate column

--- client/src/Contact.jsx
+++ client/src/Contact.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import styled from "styled-components";
 
+const ContactGrid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 2.5fr 2.5 fr 1fr;
+`;
+
+const Text = styled.span`
+  grid-column: 2/3;
+`
+
 const Titles = styled.p`
   font-family: 'Rowdies', cursive;
   font-size: 2rem;
@@ -9,17 +18,19 @@ const Titles = styled.p`
 
 const Contact = () => {
   return (
-    <div>
-      <Titles>Phone</Titles>
-      <p>(303) 330-5272</p>
-      <Titles>E-Mail</Titles>
-      <p>alexduncan1@outlook.com</p>
-      <p>monkeymedic26@gmail.com</p>
-      <Titles>LinkedIn</Titles>
-      <p>Alex Duncan</p>
-      <Titles>GitHub</Titles>
-      <p>monkeymedic26</p>
-    </div>
+    <ContactGrid>
+      <Text>
+        <Titles>Phone</Titles>
+        <p>(303) 330-5272</p>
+        <Titles>E-Mail</Titles>
+        <p>alexduncan1@outlook.com</p>
+        <p>monkeymedic26@gmail.com</p>
+        <Titles>LinkedIn</Titles>
+        <p>Alex Duncan</p>
+        <Titles>GitHub</Titles>
+        <p>monkeymedic26</p>
+      </Text>
+    </ContactGrid>
   )
 }
 
