51a590f99312ac7dde3c76a06a0ac542c74c7d93
monkeymedic26 <monkeymedic26@gmail.com>
2022-05-09 10:35:40 -0600

    added another column and adjusted paragraph sections to fit

--- client/src/Entry.jsx
+++ client/src/Entry.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 
 const Grid = styled.div`
   display: grid;
-  grid-template-columns: 1fr 5fr 1fr;
+  grid-template-columns: 1fr 2.5fr 2.5fr 1fr;
   grid-template-rows: 1fr;
 `;
 
@@ -18,14 +18,18 @@ const GridElement = styled.div`
 `;
 
 const Intro1 = styled.p`
-  grid-column: 1/2;
+  grid-column: 2/3;
+  grid-row: 3/4;
+  margin-right: 1rem;
   font-size: 1.5rem;
   font-family: 'Lato', sans-serif;
   text-align: justify;
 `;
 
 const Intro2 = styled.p`
-  grid-column: 2/3;
+  grid-column: 3/4;
+  grid-row: 3/4;
+  margin-left: 1rem;
   font-size: 1.5rem;
   font-family: 'Lato', sans-serif;
   text-align: justify;
@@ -62,12 +66,8 @@ const Entry = () => {
         <Letter>D</Letter>
         <NamePart>uncan</NamePart>
       </GridElement>
-      <GridElement>
-        <InnerGrid>
           <Intro1>Hello, World!! I am a Full-Stack Engineer and Graphic Designer. I earned my BA in Visual Communication and Design: Graphic Design at the University of Northern Colorado graduating May 2018.  I recieved my engineering training from Hack Reactor graduating April 2021. My engineering skills apply to all aspects of web development from front-end to back-end.  Where as my design skills focus more on logos and print media. I am also well versed in designing and creating 3D models for 3D printing.</Intro1>
           <Intro2>Looking for a fresh faced, new, excited, and motivated software engineer or designer? Looking for a quick learner that isn't afraid of a challenge? Then I am that person! Do you want to talk about my life or work? Shoot me a message on email or give me a call! I love talking about myself and my work, or other people and their work. I also just like to shoot the breeze from time to time. Hope to hear from you soon!</Intro2>
-        </InnerGrid>
-      </GridElement>
     </Grid>
   )
 }
