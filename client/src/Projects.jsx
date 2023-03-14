89f7846b43ce903cd5d64731e35f877f7554dc34
monkeymedic26 <monkeymedic26@gmail.com>
2023-03-14 11:37:46 -0600

    Add Title for card

--- client/src/Projects.jsx
+++ client/src/Projects.jsx
@@ -1,12 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 
-const CardLeft = styled.span`
-  grid-column: 2/3;
-  border: 2px solid #CA61E1;
-  height: 150px;
-`;
-
 const Grid = styled.div`
   display: grid;
   grid-template-columns: 1fr 2.5fr 2.5fr 1fr;
@@ -14,10 +8,22 @@ const Grid = styled.div`
   background: #28292B;
 `;
 
+const CardLeft = styled.span`
+  grid-column: 2/3;
+  border: 2px solid #CA61E1;
+  height: 150px;
+`;
+
+const Title = styled.h2`
+  color: #CA61E1;
+  margin-left: 10px;
+`;
+
 const Projects = () => {
   return (
     <Grid>
       <CardLeft>
+        <Title>Alliance Drydocks</Title>
 
       </CardLeft>
     </Grid>
