6977f6a24a0cb78f5cc9f56499feaaa6aeccdae6
monkeymedic26 <monkeymedic26@gmail.com>
2023-03-14 12:21:50 -0600

    Added info for 2 cards to the application

--- client/src/Projects.jsx
+++ client/src/Projects.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import drydocks_home from "./images/Alliance_Drydocks/drydocks_home.jpg";
 
 const Grid = styled.div`
   display: grid;
@@ -11,12 +12,32 @@ const Grid = styled.div`
 const CardLeft = styled.span`
   grid-column: 2/3;
   border: 2px solid #CA61E1;
-  height: 150px;
+  height: 250px;
 `;
 
-const Title = styled.h2`
+const CardRight = styled.span`
+  grid-column: 3/4;
+  border: 2px solid #CA61E1;
+  height: 250px;
+`;
+
+const Title = styled.h1`
   color: #CA61E1;
+  margin: 10px;
+`;
+
+const Thumbnail = styled.img`
+  height: 150px;
   margin-left: 10px;
+  display: inline-block;
+`;
+
+const Descrip = styled.p`
+  display: inline-block;
+  color: #CA61E1;
+  font-family: 'Lato', sans-serif;
+  margin: 0px 0px 0px 10px;
+  width: 300px;
 `;
 
 const Projects = () => {
@@ -24,9 +45,13 @@ const Projects = () => {
     <Grid>
       <CardLeft>
         <Title>Alliance Drydocks</Title>
-        <img src="./images/Alliance_Drydocks/drydocks_home.jpg" alt="alliance_drydocks_home_page" />
-
+        <Thumbnail src={drydocks_home} alt="alliance_drydocks_home_page" />
+        <Descrip>Alliance Drydocks is my personal pet project. It is a shipbuilder application for the online game Star Trek Online. It is still under contruction but shows some of my skills as an engineer. I created it using Javascript and React for the front end and Node for the server. It currently has no backend architecture and is loading all info from dummy data.</Descrip>
       </CardLeft>
+      <CardRight>
+        <Title>JA Scheduler</Title>
+        <Descrip>JA Scheduler is an internal application I workd on during my time with Rural Sourcing. Due to company policy I am unfortunately unable to share any images or code of this program. The application is designed to be an availablity viewer showing the availability on a 5 day calander.</Descrip>
+      </CardRight>
     </Grid>
   )
 }
