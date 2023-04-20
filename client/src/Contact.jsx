bc606fb5b04a90745b1c0c461bafaf00a8453349
monkeymedic26 <monkeymedic26@gmail.com>
2023-04-19 18:54:43 -0600

    Update tags for MaterialUI

--- client/src/Contact.jsx
+++ client/src/Contact.jsx
@@ -1,20 +1,20 @@
 import React from "react";
+import Container  from  '@mui/material/Container';
+import Typography from "@mui/material/Typography";
 
 const Contact = () => {
   return (
-    <ContactGrid>
-      <Text>
-        <Titles>Phone</Titles>
-        <Info>(303) 330-5272</Info>
-        <Titles>E-Mail</Titles>
-        <Info href="mailto: alexduncan1@outlook.com" target="_blank">alexduncan1@outlook.com</Info><br />
-        <Info href="mailto: monkeymedic26@gmail.com" target="_blank">monkeymedic26@gmail.com</Info>
-        <Titles>LinkedIn</Titles>
-        <Info href="https://www.linkedin.com/in/alexjduncan/" target="_blank">Alex Duncan</Info>
-        <Titles>GitHub</Titles>
-        <Info href="https://github.com/monkeymedic26" target="_blank">monkeymedic26</Info>
-      </Text>
-    </ContactGrid>
+    <Container>
+        <Typography variant="h4" component="h4">Phone</Typography>
+        <Typography  variant="h5" component="h5">(303) 330-5272</Typography>
+        <Typography variant="h4" component="h4">E-Mail</Typography>
+        <Typography  variant="h5" component="h5" href="mailto: alexduncan1@outlook.com" target="_blank">alexduncan1@outlook.com</Typography><br />
+        <Typography  variant="h5" component="h5" href="mailto: monkeymedic26@gmail.com" target="_blank">monkeymedic26@gmail.com</Typography>
+        <Typography variant="h4" component="h4">LinkedIn</Typography>
+        <Typography  variant="h5" component="h5" href="https://www.linkedin.com/in/alexjduncan/" target="_blank">Alex Duncan</Typography>
+        <Typography variant="h4" component="h4">GitHub</Typography>
+        <Typography  variant="h5" component="h5" href="https://github.com/monkeymedic26" target="_blank">monkeymedic26</Typography>
+    </Container>
   )
 }
 
