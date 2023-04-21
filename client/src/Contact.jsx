b52099a2d1baa19e6109c5a7e8452e5387885d90
monkeymedic26 <monkeymedic26@gmail.com>
2023-04-20 20:21:25 -0600

    Fix and test links

--- client/src/Contact.jsx
+++ client/src/Contact.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import Container  from  '@mui/material/Container';
 import Typography from "@mui/material/Typography";
+import Link from '@mui/material/Link';
 
 const Contact = () => {
   return (
-    <Container>
+    <Container sx={{display: "flex", flexFlow: "column", alignItems: "center", justifyContent: "space-around"}}>
         <Typography variant="h4" component="h4">Phone</Typography>
         <Typography  variant="h5" component="h5">(303) 330-5272</Typography>
         <Typography variant="h4" component="h4">E-Mail</Typography>
-        <Typography  variant="h5" component="h5" href="mailto: alexduncan1@outlook.com" target="_blank">alexduncan1@outlook.com</Typography><br />
-        <Typography  variant="h5" component="h5" href="mailto: monkeymedic26@gmail.com" target="_blank">monkeymedic26@gmail.com</Typography>
+        <Link  variant="h5" href="mailto: alexduncan1@outlook.com" target="_blank">alexduncan1@outlook.com</Link>
+        <Link  variant="h5" href="mailto: monkeymedic26@gmail.com" target="_blank">monkeymedic26@gmail.com</Link>
         <Typography variant="h4" component="h4">LinkedIn</Typography>
-        <Typography  variant="h5" component="h5" href="https://www.linkedin.com/in/alexjduncan/" target="_blank">Alex Duncan</Typography>
+        <Link  variant="h5" href="https://www.linkedin.com/in/alexjduncan/" target="_blank">Alex Duncan</Link>
         <Typography variant="h4" component="h4">GitHub</Typography>
-        <Typography  variant="h5" component="h5" href="https://github.com/monkeymedic26" target="_blank">monkeymedic26</Typography>
+        <Link  variant="h5" href="https://github.com/AlexJDuncan" target="_blank">AlexJDuncan</Link>
     </Container>
   )
 }
