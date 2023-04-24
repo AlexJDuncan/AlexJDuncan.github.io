6713643995d8e7a2b8da47f21cfcee5042bad801
monkeymedic26 <monkeymedic26@gmail.com>
2023-04-24 14:32:11 -0600

    Rename Overdrive Outfits images and create page for overdrive outfits

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Entry from './Entry';
 import Contact from './Contact';
 import Projects from  './Projects';
 import BottomBar from './BottomBar';
+import OverdriveOutfits from './components/Project Detail Pages/OverdriveOutfits';
 import { Container } from '@mui/material';
 
 
@@ -13,7 +14,7 @@ const App = () => {
   <Container maxWidth="xl" sx={{height: "100vh", display: "grid", gridTemplateRows: "1fr 4fr 1fr"}}>
     <Nav />
       <Routes>
-        <Route path="/" element={<Entry />} />
+        <Route path="/" element={<OverdriveOutfits />} />
         <Route path="Home" element={<Entry />} />
         <Route path="Contact" element={<Contact />} />
         <Route path="Projects" element={<Projects />} />
